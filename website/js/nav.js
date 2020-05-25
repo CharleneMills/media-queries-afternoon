666d30a8b4cbe8276da6a5c3e7a8746768cf9d84
CharleneMills <charleneskater@gmail.com>
2020-05-25 11:56:12 -0600

    Fixed the NAV!

--- website/js/nav.js
+++ website/js/nav.js
@@ -4,18 +4,27 @@
 const mobileDiv = document.getElementById("mobileDiv");
 
 function toggleMobileMenu(){
-    console.log('hello this toggled')
-    console.log(mobileDiv)
+    
+ 
+        var element = document.getElementById("mobileDiv");
+        element.classList.toggle("open");
+
+    
+    
+    
+    
+    // console.log('hello this toggled')
+    // console.log(mobileDiv)
     
 
-    if (mobileDiv.classList.contains("default") || mobileDiv.classList.contains("closed")){
-         mobileDiv.classList.add("open");
-         mobileDiv.classList.remove("default");
-         mobileDiv.classList.remove("closed");
-    } else if (mobileDiv.classList.contains("open")){
-            mobileDiv.classList.add("closed");
-            mobileDiv.classList.remove("open");
-    }   
+    // if (mobileDiv.classList.contains("default") || mobileDiv.classList.contains("closed")){
+    //      mobileDiv.classList.add("open");
+    //      mobileDiv.classList.remove("default");
+    //      mobileDiv.classList.remove("closed");
+    // } else if (mobileDiv.classList.contains("open")){
+    //         mobileDiv.classList.add("closed");
+    //         mobileDiv.classList.remove("open");
+    // }   
        
 
 };
